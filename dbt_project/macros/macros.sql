902a1dbdc4b0b733d247f4b104e487f696801686
ankurnearform <ankur.chopra@nearform.com>
2024-11-03 21:25:27 -0500

    Add generated DBT models based on JIRA https://genaiprojects.atlassian.net/browse/SCRUM-3

---
+++ dbt_project/macros/macros.sql
@@ -0,0 +1,9 @@
+{% macro calculate_total_transactions(column_name) %}
+    sum({{ column_name }})
+{% endmacro %}
+
+{% macro set_audit_columns() %}
+    CAST(current_timestamp() AS TIMESTAMP) as created_at,
+    CAST(current_timestamp() AS TIMESTAMP) as updated_at,
+    CAST(current_timestamp() AS TIMESTAMP) as processed_at
+{% endmacro %}

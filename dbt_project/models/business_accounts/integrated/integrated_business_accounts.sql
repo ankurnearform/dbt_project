d2efa83a242e0a54ab3cac4a724729f881356782
ankurnearform <ankur.chopra@nearform.com>
2024-11-07 17:44:11 -0500

    Add generated DBT models based on JIRA https://genaiprojects.atlassian.net/browse/SCRUM-3

--- dbt_project/models/business_accounts/integrated/integrated_business_accounts.sql
+++ dbt_project/models/business_accounts/integrated/integrated_business_accounts.sql
@@ -1,35 +1,38 @@
-{
+{{
+
     config(
         materialized='view',
         unique_key='account_id'
     )
-}
+}}
 
-with deduplicated_business_accounts as (
+with business_accounts as (
+    select
+        account_id,
+        account_name,
+        contact_email,
+        formatted_registration_date
+    from {{ ref('staging_business_accounts') }}
+),
+
+deduplicated_accounts as (
     select
         account_id,
         account_name,
         contact_email,
         formatted_registration_date,
         row_number() over (partition by account_id order by formatted_registration_date desc) as rn
-    from {{ ref('staging_business_accounts') }}
-)
-
-, transaction_totals as (
-    select
-        account_id,
-        sum(transaction_amount) as total_transactions_amount
-    from {{ source('raw', 'raw_transactions') }}
-    group by account_id
+    from business_accounts
 )
 
 select
-    dba.account_id,
-    dba.account_name,
-    dba.contact_email,
-    dba.formatted_registration_date,
-    coalesce(tt.total_transactions_amount, 0) as total_transactions_amount
-from deduplicated_business_accounts dba
-left join transaction_totals tt on dba.account_id = tt.account_id
-where dba.rn = 1
-order by dba.formatted_registration_date desc;
+    da.account_id,
+    da.account_name,
+    da.contact_email,
+    da.formatted_registration_date,
+    coalesce(sum(t.transaction_amount), 0) as total_transactions_amount
+from deduplicated_accounts da
+left join {{ source('raw', 'raw_transactions') }} t on da.account_id = t.account_id
+where da.rn = 1
+group by da.account_id, da.account_name, da.contact_email, da.formatted_registration_date
+order by da.formatted_registration_date desc;

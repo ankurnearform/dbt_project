07e17f0692221564e9b428544a0aa433e7498d1e
ankurnearform <ankur.chopra@nearform.com>
2024-11-09 17:12:45 -0500

    Add generated DBT models based on JIRA https://genaiprojects.atlassian.net/browse/SCRUM-2

--- dbt_project/models/business_accounts/prepared/prepared_business_accounts.sql
+++ dbt_project/models/business_accounts/prepared/prepared_business_accounts.sql
@@ -13,13 +13,13 @@ with integrated_accounts as (
         contact_email,
         registration_date,
         total_transactions_amount
-    from {{ ref('integrated.integrated_accounts') }}
+    from {{ ref('integrated.business_accounts') }}
 ),
 transaction_details as (
     select
         account_id,
-        count(*) as total_number_of_transactions,
-        sum(transaction_amount) as total_transaction_volume,
+        count(*) as total_transactions,
+        {{ calculate_total_transactions('transaction_amount') }} as total_transaction_volume,
         min(transaction_date) as first_transaction_date,
         max(transaction_date) as last_transaction_date
     from {{ source('raw', 'raw_transactions') }}
@@ -31,8 +31,7 @@ account_metrics as (
         ia.account_name,
         ia.contact_email,
         ia.registration_date,
-        ia.total_transactions_amount,
-        coalesce(td.total_number_of_transactions, 0) as total_number_of_transactions,
+        coalesce(td.total_transactions, 0) as total_transactions,
         coalesce(td.total_transaction_volume, 0) as total_transaction_volume,
         td.first_transaction_date,
         td.last_transaction_date
@@ -46,14 +45,23 @@ prepared_accounts as (
         account_name,
         contact_email,
         registration_date,
-        total_transactions_amount,
-        total_number_of_transactions,
+        total_transactions,
         total_transaction_volume,
         first_transaction_date,
         last_transaction_date,
-        {{ calculate_total_transactions('total_transaction_volume') }},
         {{ set_audit_columns() }}
     from account_metrics
 )
-select *
+select
+    account_id,
+    account_name,
+    contact_email,
+    registration_date,
+    total_transactions,
+    total_transaction_volume,
+    first_transaction_date,
+    last_transaction_date,
+    created_at,
+    updated_at,
+    processed_at
 from prepared_accounts;

25f9c8d4c603b17c8afeb3d1b00e8187179fe9d4
ankurnearform <ankur.chopra@nearform.com>
2024-11-24 20:48:29 -0500

    Add generated Changes based on JIRA https://genaiprojects.atlassian.net/browse/SCRUM-3

--- dbt_project/models/business_accounts/integrated/integrated_business_accounts.sql
+++ dbt_project/models/business_accounts/integrated/integrated_business_accounts.sql
@@ -14,42 +14,37 @@ with business_accounts as (
     from {{ source('raw', 'raw_business_accounts') }}
     where contact_email is not null
 ),
+
 deduplicated_accounts as (
-    select
-        account_id,
-        account_name,
-        contact_email,
-        registration_date,
-        row_number() over (partition by account_id order by registration_date desc) as rn
-    from business_accounts
-),
-filtered_accounts as (
-    select
+    select distinct on (account_id)
         account_id,
         account_name,
         contact_email,
         registration_date
-    from deduplicated_accounts
-    where rn = 1
+    from business_accounts
+    order by account_id, registration_date desc
 ),
-transactions as (
+
+transaction_totals as (
     select
         account_id,
         sum(transaction_amount) as total_transactions_amount
     from {{ source('raw', 'raw_transactions') }}
     group by account_id
 ),
+
 integrated_accounts as (
     select
-        fa.account_id,
-        fa.account_name,
-        fa.contact_email,
-        fa.registration_date,
-        coalesce(t.total_transactions_amount, 0) as total_transactions_amount
-    from filtered_accounts fa
-    left join transactions t
-    on fa.account_id = t.account_id
+        da.account_id,
+        da.account_name,
+        da.contact_email,
+        da.registration_date,
+        coalesce(tt.total_transactions_amount, 0) as total_transactions_amount
+    from deduplicated_accounts da
+    left join transaction_totals tt
+    on da.account_id = tt.account_id
+    order by da.registration_date desc
 )
+
 select *
-from integrated_accounts
-order by registration_date desc;
+from integrated_accounts;
--- dbt_project/models/business_accounts/prepared/prepared_business_accounts.sql
+++ dbt_project/models/business_accounts/prepared/prepared_business_accounts.sql
@@ -6,54 +6,45 @@
     )
 }}
 
-with integrated_accounts as (
-    select
-        account_id,
-        account_name,
-        contact_email,
-        registration_date,
-        total_transactions_amount
-    from {{ ref('integrated.integrated_accounts') }}
-),
-transaction_details as (
+with raw_transactions_aggregated as (
     select
         account_id,
         count(*) as total_number_of_transactions,
-        sum(transaction_amount) as total_transaction_volume,
+        {{ calculate_total_transactions('transaction_amount') }} as total_transaction_volume,
         min(transaction_date) as first_transaction_date,
         max(transaction_date) as last_transaction_date
     from {{ source('raw', 'raw_transactions') }}
     group by account_id
 ),
+
 account_metrics as (
     select
         ia.account_id,
         ia.account_name,
         ia.contact_email,
         ia.registration_date,
-        ia.total_transactions_amount,
-        coalesce(td.total_number_of_transactions, 0) as total_number_of_transactions,
-        coalesce(td.total_transaction_volume, 0) as total_transaction_volume,
-        td.first_transaction_date,
-        td.last_transaction_date
-    from integrated_accounts ia
-    left join transaction_details td
-    on ia.account_id = td.account_id
+        rta.total_number_of_transactions,
+        rta.total_transaction_volume,
+        rta.first_transaction_date,
+        rta.last_transaction_date
+    from {{ ref('integrated.integrated_accounts') }} ia
+    left join raw_transactions_aggregated rta
+    on ia.account_id = rta.account_id
 ),
-prepared_accounts as (
+
+final_output as (
     select
-        account_id,
-        account_name,
-        contact_email,
-        registration_date,
-        total_transactions_amount,
-        total_number_of_transactions,
-        total_transaction_volume,
-        first_transaction_date,
-        last_transaction_date,
-        {{ calculate_total_transactions('total_transaction_volume') }},
+        am.account_id,
+        am.account_name,
+        am.contact_email,
+        am.registration_date,
+        am.total_number_of_transactions,
+        am.total_transaction_volume,
+        am.first_transaction_date,
+        am.last_transaction_date,
         {{ set_audit_columns() }}
-    from account_metrics
+    from account_metrics am
 )
+
 select *
-from prepared_accounts;
+from final_output;

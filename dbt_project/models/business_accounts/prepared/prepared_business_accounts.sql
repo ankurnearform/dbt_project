60e7a62540aedebaa7283f00486da3d35f77bcba
ankurnearform <ankur.chopra@nearform.com>
2024-11-25 23:22:55 -0500

    Add generated Changes based on JIRA https://genaiprojects.atlassian.net/browse/SCRUM-3

--- dbt_project/models/business_accounts/prepared/prepared_business_accounts.sql
+++ dbt_project/models/business_accounts/prepared/prepared_business_accounts.sql
@@ -6,54 +6,40 @@
     )
 }}
 
-with integrated_accounts as (
-    select
-        account_id,
-        account_name,
-        contact_email,
-        registration_date,
-        total_transactions_amount
-    from {{ ref('integrated.integrated_accounts') }}
-),
-transaction_details as (
+with account_transactions as (
     select
         account_id,
         count(*) as total_number_of_transactions,
-        sum(transaction_amount) as total_transaction_volume,
+        {{ calculate_total_transactions('transaction_amount') }} as total_transaction_volume,
         min(transaction_date) as first_transaction_date,
         max(transaction_date) as last_transaction_date
     from {{ source('raw', 'raw_transactions') }}
     group by account_id
 ),
-account_metrics as (
+
+prepared_accounts as (
     select
         ia.account_id,
         ia.account_name,
         ia.contact_email,
         ia.registration_date,
-        ia.total_transactions_amount,
-        coalesce(td.total_number_of_transactions, 0) as total_number_of_transactions,
-        coalesce(td.total_transaction_volume, 0) as total_transaction_volume,
-        td.first_transaction_date,
-        td.last_transaction_date
-    from integrated_accounts ia
-    left join transaction_details td
-    on ia.account_id = td.account_id
-),
-prepared_accounts as (
-    select
-        account_id,
-        account_name,
-        contact_email,
-        registration_date,
-        total_transactions_amount,
-        total_number_of_transactions,
-        total_transaction_volume,
-        first_transaction_date,
-        last_transaction_date,
-        {{ calculate_total_transactions('total_transaction_volume') }},
-        {{ set_audit_columns() }}
-    from account_metrics
+        at.total_number_of_transactions,
+        at.total_transaction_volume,
+        at.first_transaction_date,
+        at.last_transaction_date
+    from {{ ref('integrated_accounts') }} ia
+    left join account_transactions at
+    on ia.account_id = at.account_id
 )
-select *
+
+select
+    account_id,
+    account_name,
+    contact_email,
+    registration_date,
+    total_number_of_transactions,
+    total_transaction_volume,
+    first_transaction_date,
+    last_transaction_date,
+    {{ set_audit_columns() }}
 from prepared_accounts;
